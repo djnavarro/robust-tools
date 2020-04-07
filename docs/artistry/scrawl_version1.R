939b8037880b7be4bc7acb1cfbec2afbdcfb3b5f
Danielle Navarro <d.navarro@unsw.edu.au>
2020-04-07 16:19:38 +1000

    add logical operations

--- docs/artistry/scrawl_version1.R
+++ docs/artistry/scrawl_version1.R
@@ -12,8 +12,8 @@ library(here)
 # Store the parameters that specify the piece in a list. In each case the 
 # parameters should be a positive integer
 art_par <- list(
-  seed = 15,        # seed for the random number generator
-  n_paths = 10000,   # number of distinct paths to draw
+  seed = 17,        # seed for the random number generator
+  n_paths = 1000,   # number of distinct paths to draw
   n_steps = 400,    # number of steps along each path
   sz_step = 100,   # what is the size of a typical step?
   sz_slip = 5     # what is the size of a typical slip?
